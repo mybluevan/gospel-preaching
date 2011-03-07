6860b78f87535226e9d1cdd6ca7c1b5b8efd8525
David Nichols <srv-admin@li208-176.members.linode.com>
2011-03-07 13:18:56 -0600

    Adding a standard WSGI script for deployment.

---
+++ apache/wsgi_handler.py
@@ -0,0 +1,21 @@
+import sys, os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../gospel_preaching')
+
+import settings
+
+import django.core.management
+django.core.management.setup_environ(settings)
+utility = django.core.management.ManagementUtility()
+command = utility.fetch_command('runserver')
+
+command.validate()
+
+import django.conf
+import django.utils
+
+django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
