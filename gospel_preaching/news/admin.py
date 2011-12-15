0870daa68872ca85a111e1452e60115f4b28825e
David Nichols <mybluevan@gmail.com>
2011-12-15 11:29:08 -0600

    Fixing some overlooked mistakes in the news application and activating it.

--- gospel_preaching/news/admin.py
+++ gospel_preaching/news/admin.py
@@ -2,18 +2,19 @@ from gospel_preaching.news.models import Update
 from django.contrib import admin
 
 class UpdateAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
     search_fields = ['title', 'text']
     date_hierarchy = 'date'
     list_display = ('title', 'date', 'email', 'approved')
     list_filter = ('approved')
     actions = ['mark_approved',]
     def mark_approved(self, request, queryset):
-        rows_updated = queryset.update(approved=True, ship_date=date.today())
+        rows_updated = queryset.update(approved=True)
         if rows_updated == 1:
-            message_bit = "1 order was"
+            message_bit = "1 update was"
         else:
-            message_bit = "%s orders were" % rows_updated
-        self.message_user(request, "%s successfully marked as shipped." % message_bit)
-    mark_shipped.short_description = "Mark selected orders as shipped"
+            message_bit = "%s updates were" % rows_updated
+        self.message_user(request, "%s successfully approved." % message_bit)
+    mark_shipped.short_description = "Approve selected news updates"
 
 admin.site.register(Update, UpdateAdmin)
--- gospel_preaching/news/models.py
+++ gospel_preaching/news/models.py
@@ -3,11 +3,14 @@ from tinymce import models as tinymce_models
 
 class Update(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     text = tinymce_models.HTMLField(blank=True)
     date = models.DateTimeField('date submitted', auto_now_add=True)
     email = models.EmailField()
     approved = models.BooleanField(default=False)
     class Meta:
         ordering = ['-date']
+    def get_absolute_url(self):
+        return ('news.views.detail', [self.slug])
     def __unicode__(self):
-        return self.title
+        return self.slug
