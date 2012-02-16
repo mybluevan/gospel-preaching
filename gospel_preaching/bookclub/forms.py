6fce2156fd47ff5f7b032acc2ef6b53d5e50d66e
David Nichols <mybluevan@gmail.com>
2012-02-16 12:13:43 -0600

    Updating bookclub order with integer fields for each commentary to allow ordering multiple copies.

--- gospel_preaching/bookclub/forms.py
+++ gospel_preaching/bookclub/forms.py
@@ -5,14 +5,14 @@ from django.contrib.localflavor.us.forms import USZipCodeField, USPhoneNumberFie
 
 class OrderForm(ModelForm):
     zip_code = USZipCodeField(label='ZIP Code')
-    email = forms.EmailField(label='E-mail Address')
-    phone = USPhoneNumberField(label='Phone Number')
-    commentary_mat = forms.BooleanField(required=False, label='Matthew Commentary (when it is available) - $20.00')
-    commentary_mark = forms.BooleanField(required=False, label='Mark Commentary - $15.00')
-    commentary_acts = forms.BooleanField(required=False, label='Acts Commentary - $15.00')
-    commentary_gal_eph = forms.BooleanField(required=False, label='Galatians/Ephesians Commentary - $15.00')
-    commentary_phil_thes = forms.BooleanField(required=False, label='Philippians-Thessalonians Commentary - $15.00')
-    commentary_tim_philm = forms.BooleanField(required=False, label='Timothy-Philemon Commentary - $15.00')
+    email = forms.EmailField(required=False, label='E-mail Address')
+    phone = USPhoneNumberField(required=False, label='Phone Number')
+    commentary_mat = forms.IntegerField(required=False, label='Matthew Commentary (when it is available) - $20.00', widget=forms.TextInput(attrs={'size': 2,}))
+    commentary_mark = forms.IntegerField(required=False, label='Mark Commentary - $15.00', widget=forms.TextInput(attrs={'size': 2,}))
+    commentary_acts = forms.IntegerField(required=False, label='Acts Commentary - $15.00', widget=forms.TextInput(attrs={'size': 2,}))
+    commentary_gal_eph = forms.IntegerField(required=False, label='Galatians/Ephesians Commentary - $15.00', widget=forms.TextInput(attrs={'size': 2,}))
+    commentary_phil_thes = forms.IntegerField(required=False, label='Philippians-Thessalonians Commentary - $15.00', widget=forms.TextInput(attrs={'size': 2,}))
+    commentary_tim_philm = forms.IntegerField(required=False, label='Timothy-Philemon Commentary - $15.00', widget=forms.TextInput(attrs={'size': 2,}))
     class Meta:
         model = Order
         fields = ('name', 'address', 'city', 'state', 'zip_code', 'email', 'phone', 'commentary_mat', 'commentary_mark', 'commentary_acts', 'commentary_gal_eph', 'commentary_phil_thes', 'commentary_tim_philm')
