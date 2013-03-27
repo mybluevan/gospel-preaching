ea705dc38701b3d609cc8f5edd51d1961f0e4fa3
David Nichols <mybluevan@gmail.com>
2013-03-27 01:03:37 -0500

    Adding url pattern for order list

--- gospel_preaching/bookclub/urls.py
+++ gospel_preaching/bookclub/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('bookclub.views',
     (r'^$', 'order'),
+    (r'^list/$', 'list_orders'),
     #(r'^join_complete/$', 'join_complete'),
     #(r'^ask/$', 'ask_question'),
     #(r'^(?P<pk>\d+)/$', 'detail'),
